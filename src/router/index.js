48749877aeae6fac26329f349345a48972f8be15
youqs <youqs0314@thundersoft.com>
2019-09-12 16:36:14 +0800

    内容填充

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ let routeslist = []
     // 模块路由添到路由列表
 routeslist.push({
         path: '/',
-        redirect: '/mhome',
+        redirect: '/home',
     },
     index, cellImhome)
 export default new Router({
